b520fdba1573d22471e87e7d45d52f0a1d53099f
ZulyVargas <zuly.vargas@mail.escuelaing.edu.co>
2021-10-13 20:21:09 -0500

    Actualizando PersistenceException.java

--- src/main/java/edu/eci/cvds/sampleprj/dao/PersistenceException.java
+++ src/main/java/edu/eci/cvds/sampleprj/dao/PersistenceException.java
@@ -14,7 +14,7 @@ public class PersistenceException extends Exception{
     }
 
 	public PersistenceException(String string, org.apache.ibatis.exceptions.PersistenceException e) {
-		// TODO Auto-generated constructor stub
+		super(string);
 	}
 
 }
